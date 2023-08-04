30266a9cfe7000b62fc6543cfa183469e79b5557
weizuenlintramax <weizuen.lim@lintramax.com>
2023-08-04 08:53:00 +0800

    Add project files.

---
+++ UploadLocationAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace UploadLocationAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ UploadLocationAPI/Controllers/UploadLocationController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+namespace UploadLocationAPI.Controllers
+{
+    [RoutePrefix("api/UploadLocation")]
+    public class UploadLocationController : ApiController
+    {
+        public class LocationData
+        {
+            public double Latitude { get; set; }
+            public double Longitude { get; set; }
+            public double accuracy { get; set; }
+            public DateTime date { get; set; }
+        }
+
+
+        [Route("Post")]
+        [HttpPost]
+        public IHttpActionResult Post([FromBody] LocationData location)
+        {
+            if(location == null)
+            {
+                return BadRequest("Invalid Location");
+            }
+
+            return Ok(location);
+        }
+
+        [Route("Get")]
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            return Ok(123);
+        }
+    }
+
+}
